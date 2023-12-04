9a83b4b8eb20d3240ef088c0e6540562f06fc280
Kexxshas <9017537@gmail.com>
2023-12-04 15:43:51 +0300

    Исправить prosept_scrpyt.py: использовать лемматизатор spacy

--- prosept_script.py
+++ prosept_script.py
@@ -3,11 +3,11 @@ import numpy as np
 import os
 import re
 import nltk
-from nltk.stem import WordNetLemmatizer
-
-# from nltk.corpus import stopwords as nltk_stopwords
+import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import pairwise_distances
+nlp = spacy.load("ru_core_news_sm")
+
 
 # исходим из того,что таблицы для работы к этому этапу имеются целиком
 TOP_K = 5
@@ -21,12 +21,12 @@ def preprocessing_data(marketing_dealerprice, marketing_product):
     )
     marketing_dealerprice.reset_index(drop=True, inplace=True)
     marketing_product = marketing_product.dropna(subset="name")
+    marketing_product.reset_index(drop=True, inplace=True)
     return marketing_dealerprice, marketing_product
 
 
 # обработка текста
 def lemmatize_text(text):
-    lemmatizer = WordNetLemmatizer()
     # отделение английских слов
     pattern = re.compile(r"(?<=[а-яА-Я])(?=[A-Z])|(?<=[a-zA-Z])(?=[а-яА-Я])")
     text = re.sub(pattern, " ", text)
@@ -39,13 +39,16 @@ def lemmatize_text(text):
     # соотношения объемов
     pattern2 = re.compile(r"\b\d+:\d+\s*-\s*\d+:\d+\b|\s*\d+:\d+\s*")
     text = re.sub(pattern2, " ", text)
-    return "".join(lemmatizer.lemmatize(text))
+    # лемматизация
+    spacy_results = nlp(text)
+    text = " ".join([token.lemma_ for token in spacy_results])
+    return text
 
 
 # векторизация названий
 def vectoriz(marketing_dealerprice, marketing_product):
     df_1 = marketing_dealerprice[["product_name_lem"]]
-    df_1 = df_1.rename(columns={"product_name": "name"})
+    df_1 = df_1.rename(columns={"product_name_lem": "name"})
     df_2 = marketing_product[["name_lem"]]
     df_2 = df_2.rename(columns={"name_lem": "name"})
     df = pd.concat([df_1, df_2])
